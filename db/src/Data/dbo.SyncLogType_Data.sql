e70ebb4de16c8429910a1932994934ec98cf9033
Bob Walker <bob.walker@octopus.com>
2020-05-27 14:36:53 -0500

    Adding new verbose sync log type

--- db/src/Data/dbo.SyncLogType_Data.sql
+++ db/src/Data/dbo.SyncLogType_Data.sql
@@ -1,4 +1,7 @@
 SET IDENTITY_INSERT [dbo].[SyncLogType] ON
+INSERT INTO [dbo].[SyncLogType] ([Id], [Name], [Priority]) VALUES (4, 'Verbose', 30)
+SET IDENTITY_INSERT [dbo].[SyncLogType] OFF
+SET IDENTITY_INSERT [dbo].[SyncLogType] ON
 INSERT INTO [dbo].[SyncLogType] ([Id], [Name], [Priority]) VALUES (1, 'Information', 20)
 INSERT INTO [dbo].[SyncLogType] ([Id], [Name], [Priority]) VALUES (2, 'Error', 1)
 INSERT INTO [dbo].[SyncLogType] ([Id], [Name], [Priority]) VALUES (3, 'Warning', 10)
