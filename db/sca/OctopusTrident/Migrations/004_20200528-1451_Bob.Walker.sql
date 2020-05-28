96c64cb63afcaf6234b1ea7168d29cb73cc27345
bob.walker <bob.walker@octopus.com>
2020-05-28 14:51:54 -0500

    Added a new sync log type - Verbose

---
+++ db/sca/OctopusTrident/Migrations/004_20200528-1451_Bob.Walker.sql
@@ -0,0 +1,19 @@
+﻿-- <Migration ID="e2640a5d-e5cd-4d76-be06-cd0f8472db2a" />
+GO
+
+
+SET DATEFORMAT YMD;
+
+
+GO
+PRINT (N'Add 1 row to [dbo].[SyncLogType]');
+
+SET IDENTITY_INSERT [dbo].[SyncLogType] ON;
+
+INSERT  INTO [dbo].[SyncLogType] ([Id], [Name], [Priority])
+VALUES                          (4, 'Verbose', 30);
+
+SET IDENTITY_INSERT [dbo].[SyncLogType] OFF;
+
+
+GO
