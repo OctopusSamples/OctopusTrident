03001bcfa06055a2ee7d591833910a58f4a5468e
bob.walker <bob.walker@octopus.com>
2020-05-28 09:42:43 -0500

    Adding SyncLogType table back to database

---
+++ db/sca/OctopusTrident/Migrations/003_20200528-0941_Bob.Walker.sql
@@ -0,0 +1,24 @@
+﻿-- <Migration ID="ac4b6873-2f9f-4668-93dd-df54b8102f84" />
+GO
+
+
+SET DATEFORMAT YMD;
+
+
+GO
+IF (SELECT COUNT(*)
+    FROM   [dbo].[SyncLogType]) = 0
+    BEGIN
+        PRINT (N'Add 3 rows to [dbo].[SyncLogType]');
+        SET IDENTITY_INSERT [dbo].[SyncLogType] ON;
+        INSERT  INTO [dbo].[SyncLogType] ([Id], [Name], [Priority])
+        VALUES                          (1, 'Error', 1);
+        INSERT  INTO [dbo].[SyncLogType] ([Id], [Name], [Priority])
+        VALUES                          (2, 'Warning', 10);
+        INSERT  INTO [dbo].[SyncLogType] ([Id], [Name], [Priority])
+        VALUES                          (3, 'Information', 20);
+        SET IDENTITY_INSERT [dbo].[SyncLogType] OFF;
+    END
+
+
+GO
